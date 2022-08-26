48828c962b290d5308454bf9b46d753f23197fd6
parul2515 <parul0522.be20@chitkara.edu.in>
2022-08-26 15:02:47 +0530

    2010990388 Kirti dhawan question1

--- question1.cpp
+++ question1.cpp
@@ -1,2 +1,34 @@
 #include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
+// 2010990388 Kirti Dhawan
+void printfirstrepeating(int arr[],int n){
+    int min=-1;
+    set<int> myset;
+    for(int i=n-1;i>=0;i--){
+        if(myset.find((arr[i]))!=myset.end()){
+            min=i;
+        }
+        else{
+            myset.insert((arr[i]));
+
+        }
+    }
+    if(min!=-1){
+        cout<<arr[min]<<"minimum element";
+    }
+    else{
+        cout<<"Invalid input";
+    }
+
+}
+int main(){
+    cout<<"enter the no of elements";
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+     printfirstrepeating(arr,n);
+}
--- question2.cpp
+++ question2.cpp
@@ -1,4 +1,36 @@
 #include <iostream>
 using namespace std;
-
+#include<bits/stdc++.h>
 // 2010990388 Kirti Dhawan
+void printconsecutiveintegers(int arr[],int n){
+    int start=0;
+    int end=0;
+    for(int i=1;i<n;i++){
+        if((arr[i]-arr[i-1])==1){ // check if the difference between the adjacent elements are 1 then they are consecutive 
+          start++;
+        }
+        else{
+            end=-1;
+            break;
+        }
+    }
+    if(end==0){
+        cout<<"The array contains the consecutive integers from " <<arr[0] <<"to" <<arr[start];
+    }
+    else{
+        cout<<"The array does not contain consecutive integers";
+    }
+    
+}
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+  sort(arr,arr+n);
+  printconsecutiveintegers(arr,n);
+    
+    
+}
